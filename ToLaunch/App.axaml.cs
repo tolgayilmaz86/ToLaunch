27ac0511276e979119db8579c3da2ebea76f48a7
Tolga YILMAZ <tolgayilmaz86@gmail.com>
2025-12-04 22:06:13 +0100

    Enhance tray menu with start/stop all applications

--- ToLaunch/App.axaml.cs
+++ ToLaunch/App.axaml.cs
@@ -41,10 +41,48 @@ public partial class App : Application
     }
 
     public void TrayIcon_Exit_Click(object? sender, System.EventArgs e)
-    {
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.Shutdown();
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.Shutdown();
+            }
+        }
+
+        public void TrayIcon_StartAll_Click(object? sender, System.EventArgs e)
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                var mainWindow = desktop.MainWindow;
+                if (mainWindow?.DataContext is MainWindowViewModel viewModel)
+                {
+                    // Start all enabled programs that are not running
+                    foreach (var card in viewModel.LaunchCards)
+                    {
+                        if (card.IsEnabled && !card.IsRunning)
+                        {
+                            card.StartCommand.Execute(null);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void TrayIcon_StopAll_Click(object? sender, System.EventArgs e)
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                var mainWindow = desktop.MainWindow;
+                if (mainWindow?.DataContext is MainWindowViewModel viewModel)
+                {
+                    // Stop all running programs
+                    foreach (var card in viewModel.LaunchCards)
+                    {
+                        if (card.IsRunning)
+                        {
+                            card.StartCommand.Execute(null);
+                        }
+                    }
+                }
+            }
         }
     }
-}
